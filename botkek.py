514b4c8362e9ff443c3fa031bbc0ae5828cf4b9d
Gregory Sobol <grishasobol@mail.ru>
2020-01-08 19:11:45 +0300

    new

--- botkek.py
+++ botkek.py
@@ -1,22 +1,21 @@
 import telebot
-import gevent 
 from threading import Thread
 
-TB = None
 ID = None
 
 class CianParser(Thread):
-    def __init__( self, bot, )
-    while True:
-        gevent.sleep(1)
-        if ID:
-            print ID
+    def __init__( self, bot):
+        Thread.__init__(self)
+        self.bot = bot
 
+    def run( slef):
+        while True:
+            gevent.sleep(1)
+            if ID:
+                print ID
 
 def main():
-    global TB
     bot = telebot.TeleBot("534840844:AAH-SUIsnwWIjn4IGB9UD-blwegM1iYqXRA")
-    TB = bot
 
     @bot.message_handler(commands=['start'])
     def send_welcome(message):
@@ -28,10 +27,11 @@ def main():
     def echo_all(message):
         bot.reply_to(message, message.text)
 
-    gevent.joinall([
-        gevent.spawn(bot.polling),
-        gevent.spawn(cian_parser),
-    ])
+    cian_parser = CianParser( bot )
+    cian_parser.run()
+    tb_process = Tread( target=bot.polling )
+    cian_parser.join()
+
 
 if __name__ == "__main__":
     main()
