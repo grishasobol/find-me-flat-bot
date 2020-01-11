865f34e9414993aa9ce3c74c931e4ed22288ec90
Gregory Sobol <grishasobol@mail.ru>
2020-01-11 13:55:12 +0300

    fix googapi

--- googapi.py
+++ googapi.py
@@ -9,8 +9,8 @@ class GoogleAPI:
         self.gmaps = googlemaps.Client( key=key )
 
     def get_travel_time( self, origin, destin, mode='transit', dep_time=DEFAULT_TIME):
-        directions_result = gmaps.directions( origin,
-                                              destin,
-                                              mode=mode,
-                                              departure_time=dep_time)
+        directions_result = self.gmaps.directions( origin,
+                                                   destin,
+                                                   mode=mode,
+                                                   departure_time=dep_time)
         return directions_result[0]['legs'][0]['duration']['value']
